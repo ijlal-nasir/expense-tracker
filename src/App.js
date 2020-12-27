54bce19bc487b7f353d8aa2e42c0f6e87a2b4d96
Muhammad Ijlal Nasir <ijlalfarooqi30@gmail.com>
2020-12-27 22:05:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello, world!</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello, world from surge.sh!</h1>
+		</div>
+	);
 }
 
 export default App;
